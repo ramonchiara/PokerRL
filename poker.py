758df22d069802e9bc68e94ac6f5f74e81b2c0e0
Ramon Chiara <ramonchiara@gmail.com>
2024-05-11 19:23:36 -0300

    Teste possíveis valores e naipes.

--- poker.py
+++ poker.py
@@ -1,4 +1,6 @@
 class Carta:
+    VALORES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    NAIPES = ['ouros', 'espadas', 'copas', 'paus']
 
     def __init__(self, valor, naipe):
         self._valor = valor
--- poker_test.py
+++ poker_test.py
@@ -10,6 +10,10 @@ class CartaTest(unittest.TestCase):
         self.assertEqual(2, c.valor)
         self.assertEqual('ouros', c.naipe)
 
+    def test_quero_saber_quais_valores_e_naipes_posso_usar(self):
+        self.assertEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'], Carta.VALORES)
+        self.assertEqual(['ouros', 'espadas', 'copas', 'paus'], Carta.NAIPES)
+
 
 if __name__ == '__main__':
     unittest.main()
